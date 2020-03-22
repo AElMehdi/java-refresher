8ab976ce9427ed94c6f92f239a7380a3f618344e
AElMehdi <assirelmehdi@gmail.com>
2020-03-22 09:30:39 +0100

    Java8 Core: Methods overriding/hiding rules

---
+++ src/java8/overloading_overriding/overriding/OverridingExamples.java
@@ -0,0 +1,34 @@
+package java8.overloading_overriding.overriding;
+
+import java8.overloading_overriding.overriding.hiding.HiddenGoose;
+
+public class OverridingExamples {
+
+    public static void main(String[] args) {
+        overriding();
+        hiding();
+    }
+
+    private static void overriding() {
+        Goose goose = new Goose();
+
+        System.out.println("*** Overriding Examples ***");
+        // Overrides parent method
+        goose.eat();
+
+        // Explicitly calling the parent method
+        goose.enforceFlyingLikeBird();
+
+        // It will call the child class method in both cases
+        goose.callFromBird();
+        goose.callFromGoose();
+    }
+
+    private static void hiding() {
+        HiddenGoose goose = new HiddenGoose();
+
+        System.out.println("*** Hiding Examples ***");
+        goose.callFromGoose();
+        goose.callFromBird();
+    }
+}
