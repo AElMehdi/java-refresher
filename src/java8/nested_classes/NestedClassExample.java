9008d5ffbfb186045a254aa9c63f36ae15e7d842
AElMehdi <assirelmehdi@gmail.com>
2020-03-25 21:00:48 +0100

    Java8 Core: Nested classes, deep inner class

--- src/java8/nested_classes/NestedClassExample.java
+++ src/java8/nested_classes/NestedClassExample.java
@@ -23,6 +23,15 @@ class NestedClassExample {
          System.out.println(privateValue);
       }
 
+      class DeepInnerClass {
+
+         void accessFields() {
+            System.out.println("Direct enclosing field:" + innerClassField);
+            System.out.println("First level enclosing field:" + staticPrivateValue);
+            System.out.println("First level enclosing field:" + privateValue);
+         }
+      }
+
    }
 
    public static void main(String[] args) {
@@ -33,5 +42,9 @@ class NestedClassExample {
       // Access through the instance of the enclosing class
       InnerClass innerClass = nestedClassExample.new InnerClass();
       innerClass.accessFields();
+
+      // The Inner.DeepInnerClass is necessary Java can't know where to look when it's too deep
+      InnerClass.DeepInnerClass deepInnerClass = innerClass.new DeepInnerClass();
+      deepInnerClass.accessFields();
    }
 }
