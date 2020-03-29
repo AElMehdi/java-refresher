20c6de7585ba28d43b05ffb5cbbda63c5dbf3f28
AElMehdi <assirelmehdi@gmail.com>
2020-03-29 21:44:39 +0200

    Java8 Core: Lambda expressions valid syntax

---
+++ src/java8/lambda_expression/LambdaExpression.java
@@ -0,0 +1,41 @@
+package java8.lambda_expression;
+
+import static java.lang.System.out;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class LambdaExpression {
+   private static MathOperation addition = Integer::sum;
+   private static MathOperation multiply = (x, y) -> x * y;
+
+   public static void main(String[] args) {
+      validSyntax();
+      out.println(operation(5, 4, addition));
+      out.println(operation(5, 4, multiply));
+
+   }
+
+   private static int operation(int a, int b, MathOperation operation) {
+      return operation.operate(a, b);
+   }
+
+   private interface MathOperation {
+      int operate(int a, int b);
+   }
+
+   private static void validSyntax() {
+      Consumer<String> oneParamLongSyntax = (String e) -> {
+         out.println("One Param full syntax");
+      };
+      Consumer oneParamShortSyntax = e -> out.println("Parentheses are optional if one param");
+   }
+
+   Supplier<String> noParam = () -> "Parentheses are mandatory when no param";
+
+    BiConsumer<String, Long> twoParams = (a, b) -> {
+       out.println("Two params => Parentheses are mandatory");
+       out.println("Two params => If you choose to put the type you have to put it for all params");
+    };
+}
