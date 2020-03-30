228eebba7b46f3693b71be5a1d07ae501b8732a2
AElMehdi <assirelmehdi@gmail.com>
2020-03-30 21:42:40 +0200

    Java8 Core: Run some of the existing functional interfaces

--- src/java8/lambda_expression/LambdaExpression.java
+++ src/java8/lambda_expression/LambdaExpression.java
@@ -2,8 +2,11 @@ package java8.lambda_expression;
 
 import static java.lang.System.out;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class LambdaExpression {
@@ -12,9 +15,14 @@ public class LambdaExpression {
 
    public static void main(String[] args) {
       validSyntax();
+      someOfTheExistingFunc();
+      runLambdaOperationExamples();
+
+   }
+
+   private static void runLambdaOperationExamples() {
       out.println(operation(5, 4, addition));
       out.println(operation(5, 4, multiply));
-
    }
 
    private static int operation(int a, int b, MathOperation operation) {
@@ -54,5 +62,16 @@ public class LambdaExpression {
       };
 
       twoParams.accept("input",1L);
+   }
+
+   private static void someOfTheExistingFunc() {
+      Predicate<String> isTrue = input -> input.contains("true");
+      out.println(isTrue.test("Is it true?"));
+
+      Consumer<Integer> sideEffect = input -> out.println("Call an operation on the input");
+      sideEffect.accept(10);
+
+      Supplier<List<String>> namesSupplier = () -> Arrays.asList("Yassine", "Marouane", "Youssef");
+      out.println(namesSupplier.get());
    }
 }
