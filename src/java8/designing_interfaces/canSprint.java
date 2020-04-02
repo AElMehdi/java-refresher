cb433bc7f8df73e5830a94cff3f6b160f6919695
AElMehdi <assirelmehdi@gmail.com>
2020-04-02 13:49:43 +0200

    Fix a little typo

--- src/java8/designing_interfaces/canSprint.java
+++ src/java8/designing_interfaces/canSprint.java
@@ -3,7 +3,7 @@ package java8.designing_interfaces;
 public interface canSprint extends CanWalk, canRun {
    void sprint();
 
-   // Should be overridden cause it exists in both base interfaces: canWalk, canRun
+   // Should be overridden cause it exists in both base interfaces: CanWalk, CanRun
    @Override
    default void walk() {
    }
