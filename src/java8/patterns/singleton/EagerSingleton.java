f0c1d55e13b696178a2edfbb0fdc0025d985fd01
AElMehdi <assirelmehdi@gmail.com>
2020-04-04 23:11:15 +0200

    Java8 Core: Singleton side notes on the basic example

--- src/java8/patterns/singleton/EagerSingleton.java
+++ src/java8/patterns/singleton/EagerSingleton.java
@@ -13,11 +13,15 @@ public class EagerSingleton {
       return instance;
    }
 
+   // While this allows us to have only one instance
+   // We are still not really there, we can improve
+   // this implementation with a lazy instantiation
+   // see LazySingleton class
    public static void main(String[] args) {
       EagerSingleton instance1 = getInstance();
       EagerSingleton instance2 = getInstance();
 
-      if (instance1.equals(instance)) {
+      if (instance1.equals(instance2)) {
          System.out.println("One instance is returned");
       }
    }
