b07b40aa39811c168471a580b22e5f7b741dfc77
AElMehdi <assirelmehdi@gmail.com>
2020-04-05 23:46:47 +0200

    Java8 Core: Immutable pattern with mutable field example

--- src/main/java/refresher/java8/patterns/immutable/ImmutableExample.java
+++ src/main/java/refresher/java8/patterns/immutable/ImmutableExample.java
@@ -1,5 +1,9 @@
 package refresher.java8.patterns.immutable;
 
+import static java.util.Collections.unmodifiableList;
+
+import java.util.List;
+
 public class ImmutableExample {
    // Prevent methods from being overridden: the class should be final
    // Fields must be private final (Set only using a constructor)
@@ -7,13 +11,22 @@ public class ImmutableExample {
    // Never shared reference to a mutable object
 
    private final String name;
-
-   public ImmutableExample(String name) {
-      this.name = name;
-   }
+   private final List<String> contacts;
 
    public String getName() {
       return name;
    }
-}
 
+   public List<String> getContacts() {
+      return contacts;
+   }
+
+   public ImmutableExample(String name, List<String> contacts) {
+      this.name = name;
+
+      if (contacts == null) {
+         throw new RuntimeException("Contacts is required!");
+      }
+      this.contacts = unmodifiableList(contacts);
+   }
+}
---
+++ src/test/java/refresher/java8/patterns/immutable/ImmutableExampleTest.java
@@ -0,0 +1,18 @@
+package refresher.java8.patterns.immutable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class ImmutableExampleTest {
+
+   @Test
+   void should_throw_exception_when_mutable_field_is_null() {
+      RuntimeException exception = assertThrows(RuntimeException.class,
+            () -> new ImmutableExample("name", null),
+            "A runtime exception should be thrown!");
+
+      assertEquals("Contacts is required!", exception.getMessage());
+   }
+}
