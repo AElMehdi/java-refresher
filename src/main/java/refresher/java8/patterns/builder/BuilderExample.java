6a22cfc2cc64064186503e78871b5641275b36be
AElMehdi <assirelmehdi@gmail.com>
2020-04-06 23:57:14 +0200

    Java8 Core: Builder pattern basics

--- src/main/java/refresher/java8/patterns/builder/BuilderExample.java
+++ src/main/java/refresher/java8/patterns/builder/BuilderExample.java
@@ -1,4 +1,56 @@
 package refresher.java8.patterns.builder;
 
-public class BuilderExample {
+import static java.lang.System.*;
+
+class BuilderExample {
+   private String name;
+   private int age;
+
+   private BuilderExample(Builder builder) {
+      this.name = builder.name;
+      this.age = builder.age;
+   }
+
+   public String name() {
+      return name;
+   }
+
+   public int age() {
+      return age;
+   }
+
+   @Override
+   public String toString() {
+      return "BuilderExample{" +
+            "name='" + name + '\'' +
+            ", age=" + age +
+            '}';
+   }
+
+   static class Builder {
+      private String name;
+      private int age;
+
+      Builder name(String name) {
+         this.name = name;
+         return this;
+      }
+
+      Builder age(int age) {
+         this.age = age;
+         return this;
+      }
+
+      BuilderExample build() {
+         return new BuilderExample(this);
+      }
+   }
+
+   public static void main(String[] args) {
+      BuilderExample youssef = new Builder()
+            .name("Yassine")
+            .age(25).build();
+
+      out.println(youssef);
+   }
 }
